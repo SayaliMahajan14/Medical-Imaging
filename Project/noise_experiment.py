e0fc2fc54f047e6d12b0ebce894da86aa520ea26
SayaliMahajan14 <sayalimahajan85@gmail.com>
2026-07-09 23:59:54 +0530

    Update noise_experiment.py

--- Project/noise_experiment.py
+++ Project/noise_experiment.py
@@ -12,72 +12,90 @@ tf.reset_default_graph()
 subDirectory='04Jun_0356pm_5L_10K_50E_AG'
 #%%Read the testing data from dataset.hdf5 file
 
-
-
-tstOrg,tstAtb,tstCsm,tstMask=sf.getTestingData()
-
-#you can also read more testing data from dataset.hdf5 (see readme) file using the command
-#tstOrg,tstAtb,tstCsm,tstMask=sf.getData('testing',num=100)
+noise_levels=[0.0,0.01,0.03,0.05]
+results=[]
 
 #%% Load existing model. Then do the reconstruction
-print ('Now loading the model ...')
+modelDir = cwd + '/savedModels/' + subDirectory
 
-modelDir= cwd+'/savedModels/'+subDirectory #complete path
-rec=np.empty(tstAtb.shape,dtype=np.complex64) #rec variable will have output
-
-tf.reset_default_graph()
 loadChkPoint=tf.train.latest_checkpoint(modelDir)
-config = tf.ConfigProto()
+
+config=tf.ConfigProto()
 config.gpu_options.allow_growth=True
-with tf.Session(config=config) as sess:
-    new_saver = tf.train.import_meta_graph(modelDir+'/modelTst.meta')
-    new_saver.restore(sess, loadChkPoint)
-    graph = tf.get_default_graph()
-    predT =graph.get_tensor_by_name('predTst:0')
-    maskT =graph.get_tensor_by_name('mask:0')
-    atbT=graph.get_tensor_by_name('atb:0')
-    csmT   =graph.get_tensor_by_name('csm:0')
-    wts=sess.run(tf.global_variables())
-    dataDict={atbT:tstAtb,maskT:tstMask,csmT:tstCsm }
-    rec=sess.run(predT,feed_dict=dataDict)
 
-rec=sf.r2c(rec.squeeze())
-print('Reconstruction done')
+for sigma in noise_levels:
 
-#%% normalize the data for calculating PSNR
+    print("\n==========================")
+    print("Noise sigma =",sigma)
+    print("==========================")
 
-print('Now calculating the PSNR (dB) values')
+    tstOrg,tstAtb,tstCsm,tstMask=sf.getTestingData(sigma)
 
-normOrg=sf.normalize01( np.abs(tstOrg))
-normAtb=sf.normalize01( np.abs(sf.r2c(tstAtb)))
-normRec=sf.normalize01(np.abs(rec))
+    tf.reset_default_graph()
 
-psnrAtb=sf.myPSNR(normOrg,normAtb)
-psnrRec=sf.myPSNR(normOrg,normRec)
+    with tf.Session(config=config) as sess:
 
-print ('*****************')
-print ('  ' + 'Noisy ' + 'Recon')
-print ('  {0:.2f} {1:.2f}'.format(psnrAtb,psnrRec))
-print ('*****************')
+        new_saver=tf.train.import_meta_graph(modelDir+'/modelTst.meta')
 
-#%% Display the output images
-plot= lambda x: plt.imshow(x,cmap=plt.cm.gray, clim=(0.0, .8))
-plt.clf()
-plt.subplot(141)
-plot(np.fft.fftshift(tstMask[0]))
-plt.axis('off')
-plt.title('Mask')
-plt.subplot(142)
-plot(normOrg)
-plt.axis('off')
-plt.title('Original')
-plt.subplot(143)
-plot(normAtb)
-plt.title('Input, PSNR='+str(psnrAtb.round(2))+' dB' )
-plt.axis('off')
-plt.subplot(144)
-plot(normRec)
-plt.title('Output, PSNR='+ str(psnrRec.round(2)) +' dB')
-plt.axis('off')
-plt.subplots_adjust(left=0, right=1, top=1, bottom=0,wspace=.01)
-plt.show()
+        new_saver.restore(sess,loadChkPoint)
+
+        graph=tf.get_default_graph()
+
+        predT=graph.get_tensor_by_name('predTst:0')
+
+        maskT=graph.get_tensor_by_name('mask:0')
+
+        atbT=graph.get_tensor_by_name('atb:0')
+
+        csmT=graph.get_tensor_by_name('csm:0')
+
+        rec=sess.run(
+            predT,
+            feed_dict={
+                atbT:tstAtb,
+                maskT:tstMask,
+                csmT:tstCsm
+            }
+        )
+
+    rec=sf.r2c(rec.squeeze())
+
+    normOrg=sf.normalize01(np.abs(tstOrg))
+
+    normAtb=sf.normalize01(np.abs(sf.r2c(tstAtb)))
+
+    normRec=sf.normalize01(np.abs(rec))
+
+    psnrInput=sf.myPSNR(normOrg,normAtb)
+
+    psnrRec=sf.myPSNR(normOrg,normRec)
+
+    results.append([sigma,psnrInput,psnrRec])
+
+    plt.figure(figsize=(12,4))
+
+    plt.subplot(131)
+    plt.imshow(normOrg,cmap='gray')
+    plt.axis('off')
+    plt.title("Original")
+
+    plt.subplot(132)
+    plt.imshow(normAtb,cmap='gray')
+    plt.axis('off')
+    plt.title("Input\nPSNR %.2f"%psnrInput)
+
+    plt.subplot(133)
+    plt.imshow(normRec,cmap='gray')
+    plt.axis('off')
+    plt.title("MoDL\nPSNR %.2f"%psnrRec)
+
+    plt.savefig("Result_sigma_"+str(sigma)+".png",dpi=200)
+
+    plt.show()
+
+print("\nFinal Results\n")
+
+print("Noise\tInput\tMoDL")
+
+for r in results:
+    print("%.2f\t%.2f\t%.2f"%(r[0],r[1],r[2]))
